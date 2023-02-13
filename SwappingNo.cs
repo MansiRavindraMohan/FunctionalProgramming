7651c848629f682342e16dc8a66ed82e4a26c6da
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-13 14:48:23 +0530

    Swap Two Numbers

---
+++ SwappingNo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class SwappingNo
+    {
+        public static void SwappingTheNumber()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a = " + a + " b = " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After swap a = " + a + " b = " + b);
+        }
+    }
+}
