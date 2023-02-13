c7cf0324ed2da004d9057bc93163afbca4693caa
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-13 14:42:04 +0530

    Printing Welcome message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using FunctioalProgramming;
+
+namespace FunctionalProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programming");
+            
+            VowelAndConsonant.CheckTheCharacter();
+            SwappingNo.SwappingTheNumber();
+            EvenOddNo.CheckingNoEvenOrOdd();
+            LargestNumber.FindTheLargestNumber();
+            QuotientAndRemainder.FindQuotientAndRemainder();
+        }
+    }
+}
